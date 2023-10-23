969498d3ea399c7fb9bf2d8f624dc756fd305875
Felipe Santos <akros.fms@gmail.com>
2023-10-23 10:17:57 -0300

    dinamycs components added

--- src/app/components/little-card/little-card.component.ts
+++ src/app/components/little-card/little-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-little-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./little-card.component.css']
 })
 export class LittleCardComponent implements OnInit {
+
+  @Input()
+  cardPhoto:string = ""
+  @Input()
+  cardTitle:string = ""
 
   constructor() { }
 
